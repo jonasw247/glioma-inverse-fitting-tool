7e4f4be17f45b1569580852c1f79ea4ecb7dc79e
jonasw247 <jonas.weidner@outlook.de>
2023-12-14 20:49:27 +0100

    plot

--- analysis.py
+++ analysis.py
@@ -10,14 +10,28 @@ res = np.load("/home/jonas/workspace/programs/cmaesForPhythonFWD/results/2023_12
 
 res = np.load("/home/jonas/workspace/programs/cmaesForPhythonFWD/resultsP001/2023_12_13-23_12_33_gen_10/results.npy", allow_pickle=True).item()
 res = np.load("/home/jonas/workspace/programs/cmaesForPhythonFWD/resultsP001/2023_12_13-22_19_32_gen_20/results.npy", allow_pickle=True).item()
+
+
+res=np.load("/home/jonas/workspace/programs/cmaesForPhythonFWD/resultsP001/2023_12_14-19_59_24_gen_50/results.npy", allow_pickle=True).item()
+res= np.load("/home/jonas/workspace/programs/cmaesForPhythonFWD/resultsP001/2023_12_14-20_32_11_gen_200/results.npy", allow_pickle=True).item()
+
 # %%
-res["time_min"]
+res.keys()
+
+#%%
+res["lossDir"][30]
 #%%
 
 lossPet, lossT1c, lossFlair, times = [], [], [], []
 for i in range(len(res["lossDir"])):
     lossPet_, lossT1c_, lossFlair_, times_ = [], [], [], []
     for j in range(len(res["lossDir"][i])):
+        if not res["lossDir"][i][j]["lossTotal"] <=1:
+            print("error", i, j, res["lossDir"][i][j]["lossTotal"])
+        if not res["lossDir"][i][j]["lossTotal"] >=0:
+            print("error", i, j, res["lossDir"][i][j]["lossTotal"])
+        if not res["lossDir"][i][j]["lossTotal"] == res["lossDir"][i][j]["lossTotal"]:
+            print("error", i, j, res["lossDir"][i][j]["lossTotal"])
         lossPet_.append(res["lossDir"][i][j]["lossPet"])
         lossT1c_.append(res["lossDir"][i][j]["lossT1c"])
         lossFlair_.append(res["lossDir"][i][j]["lossFlair"])
@@ -28,27 +42,35 @@ for i in range(len(res["lossDir"])):
     times.append(times_)
 
 times = np.array(times )/60
+
 #%%
-def plotValues(values, yLab):
+plt.figure(figsize=(12, 7))	
+def plotValues(values, yLab, title):
     for i in range(len((values))):
-        plt.scatter([res["nsamples"][i]]*len(values[i]), values[i], color="tab:blue")
+        plt.scatter([res["nsamples"][i]]*len(values[i]), values[i], color="tab:blue", marker=".")
     plt.ylabel(yLab)
     plt.xlabel("Generation")
+    plt.title(title)
     plt.show()
 
-plotValues(times, "time [min]")
+title = "Samples: " + np.max(res["nsamples"]).astype(str) + " - cumulative time: " + str(np.round(np.sum(times), 2)) + " - parallel time: " + str(np.round(res["time_min"], 1)) + "min"
+plotValues(times, "time [min]", title)
 
 #plotValues(lossPet, "lossPet")
 #plotValues(lossT1c, "lossT1c")
 #plotValues(lossFlair, "lossFlair")
 
 #%%
+plt.figure(figsize=(12, 7))	
+plt.title(title)
 plt.errorbar(res["nsamples"], np.mean(times, axis=1), yerr=np.std(times, axis=1), fmt='o', color='black', label="time")
 plt.ylabel("time [min]")
 plt.xlabel("nsamples")
 plt.legend()
 
 #%%
+plt.figure(figsize=(12, 7))	
+
 plt.errorbar(res["nsamples"], np.mean(lossPet, axis=1), yerr=np.std(lossPet, axis=1), label="lossPet")
 plt.errorbar(res["nsamples"], np.mean(lossT1c, axis=1), yerr=np.std(lossT1c, axis=1),   label="lossT1c")
 plt.errorbar(res["nsamples"], np.mean(lossFlair, axis=1), yerr=np.std(lossFlair, axis=1),   label="lossFlair")
@@ -56,6 +78,8 @@ plt.ylabel("loss")
 plt.xlabel("number of samples")
 plt.legend()
 #%%
+plt.figure(figsize=(12, 7))	
+
 plt.plot(res["nsamples"], res["sigmas"])
 plt.ylabel("sigma")
 plt.xlabel("number of samples")
