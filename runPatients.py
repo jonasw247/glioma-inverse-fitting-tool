8234a7b429fc8e72f92da8e94b7d285f27436dc2
jonasw247 <jonas.weidner@outlook.de>
2023-12-21 13:24:43 +0100

    run tgm

--- runPatients.py
+++ runPatients.py
@@ -46,7 +46,8 @@ def run(edema, necrotic, enhancing, affine, pet, WM, GM, resultpath):
     np.save(resultpath + "gen_"+ str(settings["generations"]) + "_results.npy", resultDict)
     cmaesForFWD.writeNii(resultTumor, path = resultpath+"gen_"+ str(settings["generations"]) +"_result.nii.gz", affine = affine)
 
-if __name__ == '__main__':
+# 18 patients
+if False: #__name__ == '__main__':
 
     for patientID in range(991,1001):
         try:
@@ -82,5 +83,51 @@ if __name__ == '__main__':
 
         datetime = time.strftime("%Y_%m_%d-%H_%M_%S")
         resultpath = "/mnt/8tb_slot8/jonas/workingDirDatasets/18_data/resultsP" + ("0000" + str(patientID))[-3:] + "/"
+        run(edema, necrotic, enhancing, affine, pet, WM, GM, resultpath)
+
+#tgm
+if __name__ == '__main__':
+
+    for patientID in range(41,100):
+        try:
+            dataPath = "/mnt/8tb_slot8/jonas/datasets/TGM/"
+
+            segm = nib.load(dataPath + "tgm/tgm" +  ("0000" + str(patientID))[-3:] +"/preop/sub-tgm"+("0000" + str(patientID))[-3:]+"_ses-preop_space-sri_seg.nii.gz")
+
+            
+
+            segmentation = segm.get_fdata()
+            affine = segm.affine
+
+            tissuePath = dataPath + "preop_s3Results/tgm"+ ("0000" + str(patientID))[-3:] + "/"
+            WM = nib.load(tissuePath + "sub-tgm"+ ("0000" + str(patientID))[-3:] +"_ses-preop_space-sri_t1_wm.nii.gz").get_fdata()
+            
+            GM = nib.load(tissuePath + "sub-tgm"+ ("0000" + str(patientID))[-3:] +"_ses-preop_space-sri_t1_gm.nii.gz").get_fdata()
+
+        
+        except:
+            print("patient not found ", patientID)
+            continue
+
+        try:
+            pet = nib.load(dataPath + "tgm/tgm" +  ("0000" + str(patientID))[-3:] +"/preop/sub-tgm"+("0000" + str(patientID))[-3:]+"_ses-preop_space-sri_fet.nii.gz").get_fdata()
+            if pet.ndim == 4:
+                pet = pet[:,:,:,0]
+            #pet = pet * brainmask
+            pet = pet / np.max(pet)
+        except: # if no pet just set it to 0
+            pet = np.zeros_like(WM)
+        
+        assert WM.shape == GM.shape == pet.shape == segmentation.shape
+
+        # different labels then other datasets
+        edema = np.logical_or(segmentation == 3, segmentation == 2)
+        necrotic = segmentation == 1
+        enhancing = segmentation == 4
+
+        #WM[segmentation >0] = 1
+
+        datetime = time.strftime("%Y_%m_%d-%H_%M_%S")
+        resultpath = "/mnt/8tb_slot8/jonas/workingDirDatasets/tgm/cma-es_results/" + ("0000" + str(patientID))[-3:] + "/"
         run(edema, necrotic, enhancing, affine, pet, WM, GM, resultpath)
 
